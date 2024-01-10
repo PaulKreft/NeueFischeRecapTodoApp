b49038534c5a1eb6044a19d2186aedb582e44b22
paulkreft <paulykreft@gmail.com>
2024-01-10 16:02:59 +0100

    extend todo by id property

--- src/main/java/de/neuefische/paulkreft/neuefischerecaptodoapp/models/Todo.java
+++ src/main/java/de/neuefische/paulkreft/neuefischerecaptodoapp/models/Todo.java
@@ -1,10 +1,20 @@
 package de.neuefische.paulkreft.neuefischerecaptodoapp.models;
 
+import lombok.With;
+
+import java.util.UUID;
+
+@With
 public record Todo(
+        String id,
         String description,
         TodoStatus status
 ) {
     public Todo(TodoRequest request) {
-        this(request.description(), TodoStatus.OPEN);
+        this(UUID.randomUUID().toString(), request.description(), request.status());
+    }
+
+    public Todo(String description) {
+        this(UUID.randomUUID().toString(), description, TodoStatus.OPEN);
     }
 }
