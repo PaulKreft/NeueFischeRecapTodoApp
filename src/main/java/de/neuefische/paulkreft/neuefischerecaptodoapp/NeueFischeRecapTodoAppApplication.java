cea5ad6c691948849d060b4e14d3a8c49adfd323
paulkreft <paulykreft@gmail.com>
2024-01-10 14:21:09 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/paulkreft/neuefischerecaptodoapp/NeueFischeRecapTodoAppApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.paulkreft.neuefischerecaptodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeueFischeRecapTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeueFischeRecapTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/paulkreft/neuefischerecaptodoapp/NeueFischeRecapTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.paulkreft.neuefischerecaptodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeueFischeRecapTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
