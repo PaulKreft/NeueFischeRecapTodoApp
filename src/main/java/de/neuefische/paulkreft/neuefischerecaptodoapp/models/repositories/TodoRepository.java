beedf843017261661a60674f5ebb05e48e4630a7
paulkreft <paulykreft@gmail.com>
2024-01-10 16:04:54 +0100

    add functionality to repo to get, delete and update todo by id

--- src/main/java/de/neuefische/paulkreft/neuefischerecaptodoapp/models/repositories/TodoRepository.java
+++ src/main/java/de/neuefische/paulkreft/neuefischerecaptodoapp/models/repositories/TodoRepository.java
@@ -1,6 +1,7 @@
 package de.neuefische.paulkreft.neuefischerecaptodoapp.models.repositories;
 
 import de.neuefische.paulkreft.neuefischerecaptodoapp.models.Todo;
+import de.neuefische.paulkreft.neuefischerecaptodoapp.models.TodoRequest;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,16 +9,39 @@ import java.util.List;
 
 @Repository
 public class TodoRepository {
-    List<Todo> todoList = new ArrayList<>();
-
+    private final List<Todo> todoList = new ArrayList<>();
 
     public List<Todo> getTodos() {
         return todoList;
+    }
+
+    public Todo getTodoById(String id) {
+        return getTodos().stream()
+                .filter(todo -> todo.id().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     public void addTodo(Todo todo) {
         todoList.add(todo);
     }
 
+    public Todo updateTodoById(String id, TodoRequest todoRequest) {
+        Todo toUpdate = getTodoById(id);
 
+        if (toUpdate == null) {
+            return null;
+        }
+
+        Todo updatedTodo = toUpdate.withDescription(todoRequest.description()).withStatus(todoRequest.status());
+
+        todoList.remove(toUpdate);
+        todoList.add(updatedTodo);
+
+        return updatedTodo;
+    }
+
+    public void removeTodoById(String id) {
+        todoList.removeIf(todo -> todo.id().equals(id));
+    }
 }
